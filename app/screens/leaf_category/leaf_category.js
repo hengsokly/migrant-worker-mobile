ad544629cc80ee18fd1f19c275d1f47984c5d53e
Sokly <hengsokly23@gmail.com>
2021-06-15 16:01:24 +0700

    Leaf category: handle display description

--- app/screens/leaf_category/leaf_category.js
+++ app/screens/leaf_category/leaf_category.js
@@ -12,15 +12,26 @@ import { StackActions } from '@react-navigation/native';
 import { withTranslation } from 'react-i18next';
 import i18n from 'i18next';
 import * as Progress from 'react-native-progress';
-
 import { Color, FontFamily, FontSize, Style } from '../../assets/stylesheets/base_style';
 import Images from '../../utils/images';
-
 import Departure from '../../models/Departure';
-
 import SoundPlayer from '../../components/sound_player';
+import RenderHtml from 'react-native-render-html';
 
-const screenHeight = Dimensions.get('screen').height;
+const tagsStyles = {
+  h2: {
+    fontFamily: FontFamily.title,
+    fontWeight: "400",
+    fontSize: 18
+  },
+  div: {
+    fontFamily: FontFamily.body,
+    fontSize: FontSize.body
+  },
+  li: {
+    marginRight: 10
+  }
+}
 
 class LeafCategory extends Component {
   constructor(props) {
@@ -37,13 +48,6 @@ class LeafCategory extends Component {
         <Text style={{fontFamily: FontFamily.title, fontSize: 18, textAlign: 'center'}}>
           {this.state.category.name}
         </Text>
-        <View style={{maxHeight: screenHeight * 0.2, borderWidth: 0,}}>
-          <ScrollView>
-            <Text style={{fontSize: FontSize.small, textAlign: 'center'}}>
-              {this.state.category.description}
-            </Text>
-          </ScrollView>
-        </View>
       </View>
     )
   }
@@ -70,9 +74,19 @@ class LeafCategory extends Component {
           style={[styles.cateImage]}
           resizeMode='contain'
         />
-        { this._renderTitle() }
 
         { this._renderPlayAudio() }
+
+        <ScrollView style={{flex:1}}>
+          { this._renderTitle() }
+          { !!this.state.category.description &&
+            <RenderHtml
+              source={{html: this.state.category.description}}
+              contentWidth={Dimensions.get('screen').width}
+              tagsStyles={tagsStyles}
+            />
+          }
+        </ScrollView>
       </View>
     );
   }
